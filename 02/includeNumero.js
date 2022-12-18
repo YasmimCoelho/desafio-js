b0abb0c0594671b514e9903def0050b24a6e1fe2
yaasmin.coelho@gmail.com <yaasmin.coelho@gmail.com>
2022-12-18 20:17:16 -0300

    alteração atv includeNumero

--- 02/includeNumero.js
+++ 02/includeNumero.js
@@ -1,51 +1,6 @@
 // Função que receba um array de números inteiros e um número inteiro
 // Deve etornar se o número passado está informado no array e se sim, em que posição.
 
-// const booksByCategory = [
-//   {
-//       category: "Riqueza",
-//       books: [
-//           {
-//               title: "Os segredos da mente milionária",
-//               author: "T. Harv Eker",
-//           },
-//           {
-//               title: "O homem mais rico da Babilônia",
-//               author: "George S. Clason",
-//           },
-//           {
-//               title: "Pai rico, pai pobre",
-//               author: "Robert T. Kiyosaki e Sharon L. Lechter",
-//           },
-//       ],
-//   },
-//   {
-//       category: "Inteligência Emocional",
-//       books: [
-//           {
-//               title: "Você é Insubstituível",
-//               author: "Augusto Cury",
-//           },
-//           {
-//               title: "Ansiedade – Como enfrentar o mal do século",
-//               author: "Augusto Cury",
-//           },
-//           {
-//               title: "Os 7 hábitos das pessoas altamente eficazes",
-//               author: "Stephen R. Covey",
-//           },
-//       ],
-//   },
-// ];
-
-// const totalCategories =  booksByCategory.length
-
-// console.log(totalCategories);
-
-// for (let category of booksByCategory) {
-//   console.log('Total de livros da categoria', category.category)
-//   console.log(category.books.length)
-// }
 let readlineSync = require('readline-sync')
 
 let array = []
